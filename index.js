6628b2e2caaa4d03a10762814f1fed796f64491b
Marie Treschow <treschow.marie@gmail.com>
2016-11-10 17:17:50 +0100

    installing express

---
+++ index.js
@@ -0,0 +1,8 @@
+const morgan = require('morgan'),
+      pug = require('pug'),
+      express = require('express'),
+      bodyParser = require('body-parser'),
+      Sequelize = require('sequelize');
+
+
+var app = express();
